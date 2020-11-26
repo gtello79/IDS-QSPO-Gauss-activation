568a3f82a3e7bff557fd05d4c6082c2d577f27d5
Nicolas Avendaño <nicolasavendanoj@gmail.com>
2020-11-26 18:21:45 -0300

    Agregada func upd_particle

--- Class/QPSO.py
+++ Class/QPSO.py
@@ -16,6 +16,11 @@ class Q_PSO:
         self.ye = None
         self.C = None
         
+    #Swarm es una matriz de tamaño (np, nh*D), cada fila de la matriz representa una matriz de pesos
+    #"estirada", si por ejemplo son 10 nodos de entrada y 20 escondidos, una matriz de pesos seria 
+    #de dimension (20,10), entonces una particula representa esta matriz como un solo vector de 
+    # 20*10 = 200 columnas y una fila, por lo tanto la matriz de la swarm es de num_particulas*200
+    #en este caso de ejemplo.
     def ini_swarm(self, num_part, num_hidden, D):  #D es el la dimension del input en este caso 40 app
         X = np.zeros((num_part,num_hidden*D),dtype=float) 
         self.np = num_part
@@ -26,8 +31,9 @@ class Q_PSO:
             wh = self.rand_w(num_hidden,D) #retorna una matriz de tamaño num_hidden*D
             a = np.reshape(wh, (1, num_hidden*D))
             X[i]= a
-        self.X = X #no se si era lo mismo que weight
+        self.X = X #no es lo mismo que weight creo
     
+    #retorna matriz de tamaño(nextNodes,currentNodes)
     def rand_w(self, nextNodes, currentNodes):
         w = np.random.random((nextNodes, currentNodes))
         x = nextNodes+currentNodes
@@ -35,14 +41,18 @@ class Q_PSO:
         w = w*2*r - r
         return w
     
+    #funcion de activacion
     def gaussian_activation(self, x_n, w_j):
-        z = np.linalg.norm(x_n - w_j) #en el codigo el profe recibe solo x_n no se de donde saca w_j
+        z = np.linalg.norm(x_n - w_j)
         return math.exp(-0.5*z*z)
         
         
     def run_QPSO(self):
+    
         return True
 
+    #esta funcion lo que hace mas o menos es recomponer las matrices de pesos de cada particula
+    #y testear su MSE
     def fitness(self, num_hidden, D, num_part, X, xe, ye): 
         w2 = np.zeros((num_hidden, D), dtype=float)
         MSE = np.zeros((num_part, 1), dtype=float)
@@ -73,6 +83,21 @@ class Q_PSO:
         hh = (H*np.transpose(H) + np.eye(L)/self.C)
         w2 = yh*np.linalg.pinv(hh)
         return w2
+    
+    #7 argumentos xdxd
+    def upd_particle(X,pBest,pFitness, gBest, gFitness, New_pFitness, newBeta, wBest):
+        for i in range(pFitness):
+            if (New_pFitness[i] < pFitness[i]):
+                pFitness[i] = New_pFitness[i]
+                pBest[i][:] = X[i, :]
+        New_gFitness = min(pFitness)
+        idx = np.argmin(pFitness)
+        if (New_gFitness < gFitness):
+            gFitness = New_gFitness
+            gBest = pBest[idx][:]
+            wBest = newBeta[idx][:]
+            
+        return pBest, pFitness, gBest, gFitness, wBest
         
         
     
--- train.py
+++ train.py
@@ -7,10 +7,16 @@ DATA_PATH = "DATA/test.txt"
 data = pd.read_csv(DATA_PATH)
 
 
+
+    
+    
 xe = data.iloc[:, 1:40]
 ye = data.iloc[:, 40]
     
 D, N = xe.shape
+
+L = 20
+C = 2
 
 xe = np.array(xe)
 ye = np.array(ye)
@@ -19,4 +25,3 @@ X0 = np.ones((D,1))
 
 Xe = np.hstack((xe, X0))
 
-D, M = Xe.shape
