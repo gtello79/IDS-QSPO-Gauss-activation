d21c8ec4a3282c922308cec917a7c225504b4440
gonzalotello79@gmail.com <gonzalotello79@gmail.com>
2020-11-26 01:09:13 -0300

    writting qpso and init_swarm

--- Class/QPSO.py
+++ Class/QPSO.py
@@ -7,6 +7,8 @@ class Q_PSO:
         self.np = None
         self.nh = None
         self.weight = None
+        #Falta algo que le de el valor a las iteraciones, ojo al tejo
+        self.maxIter = None
         
     def ini_swarm(self, nodesP, nodesH, D):
         matrix = np.zeros((NP,NH),dtype=float)
@@ -27,4 +29,7 @@ class Q_PSO:
         return w
 
     def run_QPSO(self):
+        return True
+
+    def fitness(self):
         return True
